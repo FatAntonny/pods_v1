b57702ca098a65efae4d4dc4d8c43bb50a7ea16a
FatAntonny <92764915+FatAntonny@users.noreply.github.com>
2021-10-19 04:45:41 +0300

    Update pods_map.UE4.js

--- pods_map.UE4.js
+++ pods_map.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/FatAntonny/pods_v1/79e019ec5130135a351b6b0941f6fa14610856af/UE4Game.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
